83f64bd96cf5bcea5c3bec06ada0c8882243e840
Thiago Vanzele <thiagogomes@wiseoffices.com.br>
2024-10-04 13:50:00 -0300

    Evento para cadastrar colaborador criado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador]);
+    console.log(colaboradores);
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado}/>
       
       
     </div>
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,29 +1,65 @@
+import { useState } from "react";
+import Botao from "../Botao";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
-const Formulario = () => {
-    const times = [
-        'Programação',
-        'Front-End',
-        'Data Science',
-        'Devops',
-        'UX e Design',
-        'Mobile',
-        'Inovação e Gestão'
-    ]
+const Formulario = (props) => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
 
-    return(
-        <section className="formulario">
-            <form action="">
-                <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label={"Nome"} placeholder={"Digite seu nome"} />
-                <CampoTexto label={"Cargo"} placeholder={"Digite seu cargo"} />
-                <CampoTexto label={"Imagem"} placeholder={"Digite o endereço da imagem"}/>
-                <ListaSuspensa label={'Time'} itens={times}/>
-            </form>
-        </section>
-    );
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    props.aoColaboradorCadastrado({nome, cargo, imagem, time});
+  };
+
+  return (
+    <section className="formulario">
+      <form onSubmit={aoSalvar}>
+        <h2>Preencha os dados para criar o card do colaborador</h2>
+        <CampoTexto
+          obrigatorio={true}
+          label={"Nome"}
+          placeholder={"Digite seu nome"}
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label={"Cargo"}
+          placeholder={"Digite seu cargo"}
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          label={"Imagem"}
+          placeholder={"Digite o endereço da imagem"}
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
+        <ListaSuspensa
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+          obrigatorio={true}
+          label={"Time"}
+          itens={times}
+        />
+        <Botao>Criar card</Botao>
+      </form>
+    </section>
+  );
 };
 
 export default Formulario;
