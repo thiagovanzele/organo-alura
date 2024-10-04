8b7903e8766b627a39a95ca7f0674502b703b8c8
Thiago Vanzele <thiagogomes@wiseoffices.com.br>
2024-10-04 10:46:53 -0300

    Component Campo Texto criado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Banner from './components/Banner/Banner';
+import Banner from './components/Banner';
+import CampoTexto from './components/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label={'Nome'} placeholder={'Digite seu nome'} />
+      <CampoTexto label={'Cargo'} placeholder={'Digite seu cargo'}/>
+      <CampoTexto label={'Imagem'} placeholder={'Digite o endereço da imagem'}/>
       
     </div>
   );
